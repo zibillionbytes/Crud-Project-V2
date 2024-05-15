a535fab69f0a46d3bd34d91e07ebb31156bf677d
zibillionbytes <f.alkan.93@outlook.com>
2024-05-15 13:42:18 +0300

    Added controller and communicating service between controller and repository

--- src/main/java/com/test/crudv2/Employee.java
+++ src/main/java/com/test/crudv2/Employee.java
@@ -13,7 +13,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private String surname;
+    private String firstName;
+    private String lastName;
     private String email;
 }
---
+++ src/main/java/com/test/crudv2/EmployeeService.java
@@ -0,0 +1,32 @@
+package com.test.crudv2;
+
+import com.test.crudv2.Employee;
+import com.test.crudv2.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    public Employee saveEmployee(Employee employeee){
+        return employeeRepository.save(employeee);
+    }
+    public List<Employee> getAllEmployee(){
+        return employeeRepository.findAll();
+    }
+    public Employee getEmployeeById(Long employeeId){
+        return employeeRepository.findById(employeeId).get();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+    public void deleteEmployeeById(Long employeeID){
+        employeeRepository.deleteById(employeeId);
+    }
+   /*---------------------------------------------*/
+    /*the errors will be fixed in future commits*/
+   /*---------------------------------------------*/
+}
